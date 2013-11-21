717990224a538e5442a2c726245139cf6b21bc51
Diogo Biazus <diogob@gmail.com>
2013-11-21 12:11:45 -0200

    removed cache headers

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php 
-header('Expires: Thu, 01-Jan-70 00:00:01 GMT');
-header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
-header('Cache-Control: no-store, no-cache, must-revalidate');
-header('Cache-Control: post-check=0, pre-check=0', false);
-header('Pragma: no-cache');
 include_once("home.html"); 
 ?>
