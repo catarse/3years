d0316ddb92c6ad6e2264079db5c37b5308a31352
Diogo Biazus <diogob@gmail.com>
2014-01-07 11:44:27 -0500

    testing iponey

--- js/loader.js
+++ js/loader.js
@@ -9,7 +9,30 @@ function Loader(){
     });
   }
 
-  that.loadFile('slides_0_3');
+  that.loadFile('slides_0_3', function(){
+    Reveal.initialize({
+      width: 1276,
+      height: 638,
+      controls: false,
+      progress: false,
+      history: false,
+      center: true,
+      keyboard: true,
+      overview: false,
+
+      theme: Reveal.getQueryHash().theme,
+      transition: Reveal.getQueryHash().transition || 'default',
+
+      dependencies: [
+        { src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
+        { src: 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+        { src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+        { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+        { src: 'plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
+        { src: 'plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } }
+      ]
+    });
+  });
   /*
   that.loadFile('slides_0_3', function(){
     that.loadFile('slides_4_7', function(){
